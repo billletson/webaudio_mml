7ee98a4260791e5ca85c42792c5d0e3f89137d63
billletson <bill.letson@gmail.com>
2014-09-14 23:55:05 -0400

    implement arbitrary octave switch

--- README.md
+++ README.md
@@ -5,14 +5,13 @@ Implementation of Music Macro Language, played with the webaudio api
 
 
 Currently implemented:
-Notes including sharps and flats
-Rests
-Octave increment/decrement
+Notes including sharps and flats  
+Rests  
+Octave changes  
 
-Not Implemented:
-Dotted notes
-Arbitrary octave switch
-Change of default note length
-Change of volume
-Change of tempo
-Tracks
+Not Implemented:  
+Dotted notes  
+Change of default note length  
+Change of volume  
+Change of tempo  
+Tracks  
--- mml_audio.js
+++ mml_audio.js
@@ -2,46 +2,70 @@
 
 var context = new AudioContext();
 var BASE = 440;
-var current_octave = 4;
+var DEFAULT_OCTAVE = 4;
 var WHOLE_NOTE = 4*60/100;
 var INTER_NOTE = WHOLE_NOTE / 64;
 var STATES = { OPEN: -1, NOTE : 0, REST: 1, OCTAVE: 2 };
 var NOTES = {};
-NOTES['c'] = half_steps(BASE, -9)
-NOTES['c+'] = NOTES['d-'] = half_steps(BASE, -8)
-NOTES['d'] = half_steps(BASE, -7)
-NOTES['d+'] = NOTES['e-'] = half_steps(BASE, -6)
-NOTES['e'] = half_steps(BASE, -5)
-NOTES['f'] = half_steps(BASE, -4)
-NOTES['f+'] = NOTES['g-'] = half_steps(BASE, -3)
-NOTES['g'] = half_steps(BASE, -2)
-NOTES['g+'] = NOTES['a-'] = half_steps(BASE, -1)
+NOTES['c'] = half_steps(BASE, -9);
+NOTES['c+'] = NOTES['d-'] = half_steps(BASE, -8);
+NOTES['d'] = half_steps(BASE, -7);
+NOTES['d+'] = NOTES['e-'] = half_steps(BASE, -6);
+NOTES['e'] = half_steps(BASE, -5);
+NOTES['f'] = half_steps(BASE, -4);
+NOTES['f+'] = NOTES['g-'] = half_steps(BASE, -3);
+NOTES['g'] = half_steps(BASE, -2);
+NOTES['g+'] = NOTES['a-'] = half_steps(BASE, -1);
 NOTES['a'] = BASE;
-NOTES['a+'] = NOTES['b-'] = half_steps(BASE, 1)
-NOTES['b'] = half_steps(BASE, 2)
+NOTES['a+'] = NOTES['b-'] = half_steps(BASE, 1);
+NOTES['b'] = half_steps(BASE, 2);
+var scale = {notes : NOTES,
+             octave : DEFAULT_OCTAVE,
+             set_octave : function(target) {
+                for (var note in this.notes) {
+                    this.notes[note] =  this.notes[note] * Math.pow(2, target - this.octave);
+                }
+                this.octave = target;
+             },
+             increment : function() {
+                this.set_octave(this.octave + 1);
+             },
+             decrement : function() {
+                this.set_octave(this.octave - 1);
+             }
+            };
 
+function Token(type, name, value) {
+	this.type = typeof type !== 'undefined' ? type : STATES.OPEN;
+	this.name = typeof name !== 'undefined' ? name : "";
+	this.value = typeof value !== 'undefined' ? value : 1;
+}
 
 function tokenize(string) {
     var tokens = [];
-    var current_token = {type : STATES.OPEN, name : "", value: 1};
+    var current_token = new Token();
     var state = STATES.OPEN;
     var numeric_string = "";
     for (var i=0; i<string.length; i++) {
         var c = string.charAt(i);
         if (state === STATES.OPEN) {
             if (c == ">") {
-                tokens.push({type : STATES.OCTAVE, name : "", value: 1});
+                tokens.push(new Token(STATES.OCTAVE, "increment", 0));
             } else if (c === "<") {
-                tokens.push({type : STATES.OCTAVE, name : "", value: -1});
+                tokens.push(new Token(STATES.OCTAVE, "decrement", 0));
             } else if (c === "r") {
                 current_token.type = STATES.REST;
                 state = STATES.REST;
+            } else if (c === "o") {
+                current_token.type = STATES.OCTAVE;
+                current_token.name = "set";
+                state = STATES.OCTAVE;
             } else if (c in NOTES) {
                 current_token.type = STATES.NOTE;
                 current_token.name = c;
                 state = STATES.NOTE;
             }
-        } else if (state === STATES.REST || state === STATES.NOTE) {
+        } else if (state === STATES.REST || state === STATES.NOTE || state === STATES.OCTAVE) {
             if (c === "+" || c === "-") {
                 current_token.name += c
             } else if (isNaN(c)) {
@@ -50,7 +74,7 @@ function tokenize(string) {
                     numeric_string = "";
                 }                
                 tokens.push(current_token);
-                current_token = {type : STATES.OPEN, name : "", value: 1};
+                current_token = new Token();
                 i--; // Reprocess this character
                 state = STATES.OPEN;
             } else {
@@ -58,7 +82,7 @@ function tokenize(string) {
             }
         }
     }
-    if(state === STATES.REST || state === STATES.NOTE) {
+    if(state === STATES.REST || state === STATES.NOTE || STATES.OCTAVE) {
         if (numeric_string) {
             current_token.value = parseInt(numeric_string);
         }
@@ -70,7 +94,6 @@ function tokenize(string) {
 function play_tokens(tokens) {
     var time_position = context.currentTime;
     for (var i=0;i<tokens.length;i++) {
-        //console.log(NOTES);
         switch (tokens[i].type) {
             case STATES.NOTE:
                 play(NOTES[tokens[i].name], time_position, WHOLE_NOTE / tokens[i].value);
@@ -80,47 +103,31 @@ function play_tokens(tokens) {
                 time_position += WHOLE_NOTE / tokens[i].value;
                 break;
             case STATES.OCTAVE:
-                convert_octave(tokens[i].value);
+                switch (tokens[i].name) {
+                    case "increment":
+                        scale.increment();
+                        break;
+                    case "decrement":
+                        scale.decrement();
+                        break;
+                    case "set":
+                        scale.set_octave(tokens[i].value);
+                        break;
+                    default:
+                        console.log("Bad Token:");
+                        console.log(tokens[i]);
+                }
                 break;
             default:
                 console.log("Bad Token:");
                 console.log(tokens[i]);
         }
     }
-    set_octave(4);
+    scale.set_octave(DEFAULT_OCTAVE);
 }
 
 function play_string(string) {
     play_tokens(tokenize(string));
-}
-              
-function octave(frequency, direction) {
-    return frequency * Math.pow(2, direction);
-}
-
-function convert_octave(direction) {
-    for (var note in NOTES) {
-        NOTES[note] = octave(NOTES[note], direction);
-    }
-    current_octave += direction;
-}
-
-function set_octave(target) {
-    while (current_octave != target) {
-        if (target > current_octave) {
-            convert_octave(1);
-        } else if (target < current_octave) {
-            convert_octave(-1);
-        }
-    }
-}
-
-function perfect_fifth(frequency) {
-    return frequency * 3/2;
-}
-
-function major_third(frequency) {
-    return frequency * 4/3;
 }
 
 function half_steps(frequency, steps) {
